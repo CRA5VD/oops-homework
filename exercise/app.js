5898396aefdf398492ba5ad355c82a5e17ae011f
Connie <constancecrawford87@gmail.com>
2021-10-21 09:03:21 -0400

    OOP

---
+++ exercise/app.js
@@ -0,0 +1,21 @@
+var a = 10
+var b = 20
+var c = 30
+var d = 40
+var e = 15
+var g = 5
+var h = 25
+var j = 35
+var k = 45
+var l = 55
+
+console.log(a+b);
+console.log(k+b);
+console.log(d-e);
+console.log(l-a);
+console.log(g*h);
+console.log(a*b);
+console.log(a/b);
+console.log(a/b);
+console.log(a++b);
+console.log(a--b);
---
+++ oopHomework/app.js
@@ -0,0 +1,16 @@
+let myArray = [
+    [1,2],
+    [3,4],
+    [5,6],
+    [7,8]
+];
+console.log(myArray);
+
+var arr = [
+    [1,2], [3,4], [5,6]
+];
+for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j]);
+    }
+}
